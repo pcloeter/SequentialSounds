4b492b39992fa45304cff420359def6501e8932d
Paul Cloeter <pcloeter@gmail.com>
2019-02-06 16:54:02 -0500

    Progress on Sequencer

---
+++ src/sequencer.js
@@ -0,0 +1,25 @@
+class Sequencer {
+  constructor () {
+    this.sequencer = [
+      this.makeRow(sound1),
+      this.makeRow(sound2),
+      this.makeRow(sound3),
+      this.makeRow(sound4),
+      this.makeRow(sound5),
+      this.makeRow(sound8),
+    ]
+    // this.measuresLength = document.getElementById('measures-length');
+    
+  }
+
+  makeRow(sound) {
+    let beats = [];
+    for (let i = 0; i < 16; i++) {
+      beats.push(false)
+      
+    }
+  }
+
+  resetSequencer()
+
+}
