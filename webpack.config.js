75383d4c616ea306819bcda36a9a8e6eebda1416
Paul Cloeter <pdcloeter@gmail.com>
2019-02-06 13:18:52 -0500

    Setup webpack npm

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/app.js',
+  output: {
+      // path: app/assets/javascripts,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
+
