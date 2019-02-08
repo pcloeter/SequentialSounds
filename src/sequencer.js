1852b950b3a0ff7333849fd5dfa47732173be28b
Paul Cloeter <pcloeter@gmail.com>
2019-02-08 11:41:14 -0500

    Construct Sequencer

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,14 +1,16 @@
-import 
+import Note from './note';
 
 class Sequencer {
   constructor () {
     this.sequencer = [
+      this.makeRow("sound0"),
       this.makeRow("sound1"),
       this.makeRow("sound2"),
       this.makeRow("sound3"),
       this.makeRow("sound4"),
       this.makeRow("sound5"),
-      this.makeRow("sound8"),
+      this.makeRow("sound6"),
+      this.makeRow("sound7"),
     ]
     // this.measuresLength = document.getElementById('measures-length');
     
@@ -17,11 +19,12 @@ class Sequencer {
   makeRow(sound) {
     let notes = [];
     for (let i = 0; i < 16; i++) {
-      notes.push(false)
-      
+      notes.push(new Note(false, i, sound))
     }
+    return notes
   }
 
+  
   resetSequencer()
 
 }
