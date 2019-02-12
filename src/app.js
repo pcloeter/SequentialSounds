389979429c4566939377ff512717480c8855a01f
Paul Cloeter <pcloeter@gmail.com>
2019-02-11 20:29:28 -0500

    Debug Tone.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import Structure from './structure.js';
 import * as Tone from 'tone';
 import Sequencer from './sequencer';
 
-document.addEventListener("DOMContentLoaded", ()  => {
-  const structure = new Structure();
-  const sequencer = new Sequencer(structure);
-  sequencer.startPlayback();
+// const structure = new Structure();
+// const sequencer = new Sequencer(structure);
+// sequencer.startPlayback();
 
-  window.rowPlayback = structure.rowPlayback;
-  window.soundRowsPlayback = structure.soundRowsPlayback;
-  window.soundRows = structure.soundRows;
+// window.rowPlayback = structure.rowPlayback;
+// window.soundRowsPlayback = structure.soundRowsPlayback;
+// window.soundRows = structure.soundRows;
+document.addEventListener("DOMContentLoaded", ()  => {
+  
+  var synth = new Tone.Synth().toMaster();
+  synth.triggerAttackRelease('E4', '8n')
+  
 });
